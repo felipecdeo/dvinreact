40eb284283adcccacbfd5c71c5e1be146af79372
PinhoOn <pinhoonjob@gmail.com>
2021-08-17 06:41:46 -0300

    Hotfix for -Work- box

--- src/components/RowDif.jsx
+++ src/components/RowDif.jsx
@@ -8,8 +8,8 @@ import links from '../data/links'
         <a className="font--desc no-margin" href={i.link} target='_blank' rel="noopener">
           {i.name}
         </a>
-        <span className="font--light"> (May 2021 / Jun 2021) </span>
-        <p className="container__desc font--minimum"> Developed a freelance landing page for a coffee store. </p>
+        <span className="font--light"> {i.time} </span>
+        <p className="container__desc font--minimum"> {i.desc} </p>
       </>
     })
   }
