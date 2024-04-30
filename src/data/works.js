a7a7f150d92088750210eee6a2ee834100da8950
FelipeCosta <devfelipedeo@gmail.com>
2024-04-30 20:42:10 -0300

    feat: update cv

--- src/data/works.js
+++ src/data/works.js
@@ -3,5 +3,6 @@ export default [
   {link:"https://aceitacafe.com.br/", name:'· Aceita Café', time:'(May 2021 / Jun 2021)', desc:'Developed a freelance landing page for a coffee store.'},
   {link:"https://biosinergia.com.br/", name:'· Biosinergia', time:'(Jun 2021 / Jul 2021)', desc:'Developed a freelance landing page for a composting company.'},
   {link:"https://nomadfy.github.io/", name:'· Nomadfy', time:'(Jul 2021 / Aug 2021)', desc:'Developed a freelance webapp for a travel company.'},
-  {link:'', name: '· FKGrupo', time: '(Out 2021 / Out 2022)', desc: 'Migrated a desktop software for a website to improve workflow using TDD and SOLID methods with typescript in REACT.'}
+  {link:'', name: '· FKGrupo', time: '(Out 2021 / Out 2022)', desc: 'Migrated a desktop software for a website to improve workflow using TDD and SOLID methods with typescript in REACT.'},
+  {link:'', name: '· FKGrupo', time: '(Out 2023 / Now)', desc: "Migrated a desktop software for a website to improve workflow using TDD and SOLID methods with typescript in REACT. I've create they new Ecommerce, that's not in the air already for internal reasons, so I can't show, but its a pretty job. Made some Apps for mobile Android/IOS, just for internal usage."}
 ]
--- src/index.js
+++ src/index.js
@@ -22,7 +22,10 @@ reactDOM.render(
     </div>
     <div className="container">
     <AboutMe title="About me" desc="I’m a brazilian front-end dev looking for my first experience in a full time job."/>
-    <Skills title="Skills" desc="HTML, Pug, CSS, Sass / SCSS, Typescript / TDD, SOLID / RecoilJS, Jest / Vue, React / ES6, Json & Git / Github"/>
+    <Skills 
+      title="Skills" 
+      desc="HTML, Pug, CSS, Sass, SCSS, Typescript,TDD, SOLID, RecoilJS, ReduxJS, Vue, React, ReactNative, Angular, ES6, Git"
+    />
       <div className="container__block"> 
         <div className="container__image container__image--about"></div>
         <h1 className="container__title font--medium no-margin">Work Experience</h1>
